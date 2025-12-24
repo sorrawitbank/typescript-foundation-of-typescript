b7e65961ed038e20db2fcf01b5d4ca1b338da095
Bank <bank23113@gmail.com>
2025-12-24 13:45:49 +0700

    feat: finish ex3

--- src/ex-3.ts
+++ src/ex-3.ts
@@ -1,4 +1,14 @@
-const ordersSample = [
+type OrderStatus = "pending" | "shipped" | "delivered" | "cancelled";
+interface Order {
+  readonly orderId: string;
+  status: OrderStatus;
+  productName: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+}
+
+const ordersSample: Order[] = [
   {
     orderId: "ORD001",
     status: "pending",
@@ -94,15 +104,29 @@ const ordersSample = [
 ];
 
 //Start Coding Here
+function totalByStatusWithDiscount(
+  orders: Order[],
+  targetStatus: OrderStatus
+): number {
+  return orders
+    .filter((order) => order.status === targetStatus)
+    .reduce(
+      (acc, cur) =>
+        cur.discount
+          ? acc + cur.price * cur.quantity - cur.discount
+          : acc + cur.price * cur.quantity,
+      0
+    );
+}
 
-const totalShipped = totalByStatusWithDiscount(orders, "shipped");
+const totalShipped = totalByStatusWithDiscount(ordersSample, "shipped");
 console.log("Total for shipped orders with discount:", totalShipped);
 
-const totalPending = totalByStatusWithDiscount(orders, "pending");
+const totalPending = totalByStatusWithDiscount(ordersSample, "pending");
 console.log("Total for pending orders:", totalPending);
 
-const totalDelivered = totalByStatusWithDiscount(orders, "delivered");
+const totalDelivered = totalByStatusWithDiscount(ordersSample, "delivered");
 console.log("Total for delivered orders:", totalDelivered);
 
-const totalCancelled = totalByStatusWithDiscount(orders, "cancelled");
+const totalCancelled = totalByStatusWithDiscount(ordersSample, "cancelled");
 console.log("Total for cancelled orders:", totalCancelled);
