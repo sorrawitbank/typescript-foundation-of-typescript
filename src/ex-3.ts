47f4261cb70a865d7573b57d4f823de5c6be4aae
mrbuba <wwpptew101@gmail.com>
2025-12-24 15:19:55 +0700

    feat: add ans to ex1-ex4.2 .ts fiels

--- src/ex-3.ts
+++ src/ex-3.ts
@@ -1,4 +1,4 @@
-const ordersSample = [
+const ordersSample: Order[] = [
   {
     orderId: "ORD001",
     status: "pending",
@@ -94,15 +94,36 @@ const ordersSample = [
 ];
 
 //Start Coding Here
+type OrderStatus = "pending" | "shipped" | "delivered" | "cancelled";
 
-const totalShipped = totalByStatusWithDiscount(orders, "shipped");
+type Order = {
+  orderId: string;
+  status: OrderStatus;
+  productName: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+};
+
+function totalByStatusWithDiscount(
+  orders: Order[],
+  targetStatus: OrderStatus
+): number {
+  return orders
+    .filter((order) => order.status === targetStatus)
+    .reduce((acc, order) => {
+      return acc + order.price * order.quantity - (order.discount ?? 0);
+    }, 0);
+}
+
+const totalShipped = totalByStatusWithDiscount(ordersSample, "shipped");
 console.log("Total for shipped orders with discount:", totalShipped);
 
-const totalPending = totalByStatusWithDiscount(orders, "pending");
+const totalPending = totalByStatusWithDiscount(ordersSample, "pending");
 console.log("Total for pending orders:", totalPending);
 
-const totalDelivered = totalByStatusWithDiscount(orders, "delivered");
+const totalDelivered = totalByStatusWithDiscount(ordersSample, "delivered");
 console.log("Total for delivered orders:", totalDelivered);
 
-const totalCancelled = totalByStatusWithDiscount(orders, "cancelled");
+const totalCancelled = totalByStatusWithDiscount(ordersSample, "cancelled");
 console.log("Total for cancelled orders:", totalCancelled);
--- src/ex-4.2.ts
+++ src/ex-4.2.ts
@@ -1,8 +1,9 @@
 function processData(data: unknown): string {
-  return data.toUpperCase();
+  return typeof data === "string" ? data.toUpperCase() : "Not a string";
 }
 
 console.log(processData("hello")); // ควรได้ "HELLO"
 console.log(processData(123)); // ควรได้ "Not a string"
 
 // Error ที่เจอคือ
+/* TypeScript ไม่สามารถรู้ได้ว่าจริงแล้ว ๆ data เป็น type อะไรแน่ ๆ เพราะเราให้ data type = unknown, TypeScript จะไม่ยอมให้เอาไปใช้กับ method ได้เลยถ้าไม่ตั้งเงื่อนไขเช็ค type ให้ก่อน*/
